a7bfbbbdc1dfe7e6a9b83eda0eef456215648568
oldmud0 <oldmud0@users.noreply.github.com>
2017-08-03 21:29:33 -0500

    Implemented token reader using regex-based lexer

--- src/com/google/codeu/mathlang/impl/MyTokenReader.java
+++ src/com/google/codeu/mathlang/impl/MyTokenReader.java
@@ -15,32 +15,95 @@
 package com.google.codeu.mathlang.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.google.codeu.mathlang.core.tokens.NameToken;
+import com.google.codeu.mathlang.core.tokens.NumberToken;
+import com.google.codeu.mathlang.core.tokens.StringToken;
+import com.google.codeu.mathlang.core.tokens.SymbolToken;
 import com.google.codeu.mathlang.core.tokens.Token;
 import com.google.codeu.mathlang.parsing.TokenReader;
 
 // MY TOKEN READER
 //
 // This is YOUR implementation of the token reader interface. To know how
-// it should work, read src/com/google/codeu/mathlang/parsing/TokenReader.java.
+// it should work, read {@link com.google.codeu.mathlang.parsing.TokenReader}.
 // You should not need to change any other files to get your token reader to
-// work with the test of the system.
+// work with the rest of the system.
 public final class MyTokenReader implements TokenReader {
 
+  private String source;
+  private final List<TokenFactory<?>> tokenList = new ArrayList<>();
+
   public MyTokenReader(String source) {
-    // Your token reader will only be given a string for input. The string will
-    // contain the whole source (0 or more lines).
+    // warning, extreme lookahead sorcery, touch at your own risk
+    tokenList.add(new TokenFactory<NameToken>("^[a-zA-Z]\\w*(?=(?:[^'\"]|[\"'][^'\"]*[\"'])*$)") {
+      @Override
+      public NameToken createToken(String object) {
+        return new NameToken(object);
+      }
+    });
+    tokenList.add(new TokenFactory<StringToken>("^\\\".+?[^\\\\]\\\"") {
+      @Override
+      public StringToken createToken(String object) {
+        return new StringToken(object.substring(object.indexOf('"') + 1, object.lastIndexOf('"')));
+      }
+    });
+    tokenList.add(new TokenFactory<NumberToken>("^\\d+") {
+      @Override
+      public NumberToken createToken(String object) {
+        return new NumberToken(Double.parseDouble(object));
+      }
+    });
+    tokenList.add(new TokenFactory<SymbolToken>("^[^\\w\\s]") {
+      @Override
+      public SymbolToken createToken(String object) {
+        return new SymbolToken(object.charAt(0));
+      }
+    });
+    this.source = source;
   }
 
+  // Most of your work will take place here. For every call to |next| you should
+  // return a token until you reach the end. When there are no more tokens, you
+  // should return |null| to signal the end of input.
+  // If for any reason you detect an error in the input, you may throw an
+  // IOException which will stop all execution.
   @Override
   public Token next() throws IOException {
-    // Most of your work will take place here. For every call to |next| you should
-    // return a token until you reach the end. When there are no more tokens, you
-    // should return |null| to signal the end of input.
+    try {
+      for (TokenFactory<?> factory : tokenList) {
+        Matcher tokenMatcher = factory.getMatcher(source);
+        if (tokenMatcher.find()) {
+//        System.out.printf("%s: %s%n", factory.getClass(), tokenMatcher.group());
+//        System.out.println(source);
+          source = source.substring(tokenMatcher.end()).trim();
+//        System.out.println(source);
+          return factory.createToken(tokenMatcher.group());
+        }
+      }
+      return null;
+    } catch (Exception e) {
+      throw new IOException(e);
+    }
+  }
 
-    // If for any reason you detect an error in the input, you may throw an IOException
-    // which will stop all execution.
+  private static abstract class TokenFactory<T extends Token> {
+    /** Holds a regex pattern that corresponds to the format of the token. */
+    private Pattern pattern;
 
-    return null;
+    public TokenFactory(String pattern) {
+      this.pattern = Pattern.compile(pattern);
+    }
+
+    public Matcher getMatcher(String text) {
+      return pattern.matcher(text);
+    }
+
+    public abstract T createToken(String object);
+
   }
 }
